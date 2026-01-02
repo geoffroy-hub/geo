bda068b8e42d0e5b4a2a52fc945c00da133bbdc1
geoffroy-hub <abotchigeoffroy1@gmail.com>
2026-01-02 09:59:34 +0000

    Update admin.php
    C4

--- admin/admin.php
+++ admin/admin.php
@@ -7,8 +7,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <!-- PASSWORD AND USERNAME FOR ADMIN PANEL-->
-        <?php $pwd = 'deepti';
-              $username = 'deepti';   
+        <?php $pwd = 'Geoffroy';
+              $username = 'Kodjo@2009';   
               session_start();
         ?> 
     </head>
